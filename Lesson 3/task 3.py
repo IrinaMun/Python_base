ed26de2756b38cdc2d04ada69cc1c35705bcde92
Irina <chibi85moon@gmail.com>
2020-12-24 00:52:51 +0300

    Выполнение третьего урока

---
+++ Lesson 3/task 3.py
@@ -0,0 +1,15 @@
+# Реализовать функцию my_func(), которая принимает три позиционных аргумента, и возвращает сумму наибольших двух
+# аргументов.
+def my_func(x, y, z):
+    list = [x, y, z]
+
+    max_1 = max(list)
+    list.remove(max_1)
+    max_2 = max(list)
+    print(f'{max_1} + {max_2} = {max_1 + max_2}')
+
+
+
+my_func(float(input("Введите первое число: ")),
+        float(input("Введите второе число: ")),
+        float(input("Введите третье число: ")))
