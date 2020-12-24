9fb3a800da5c10ccf0411e9574f8fff70f2f4b48
Irina <chibi85moon@gmail.com>
2020-12-24 13:14:59 +0300

    Программа принимает действительное положительное число x и целое отрицательное число y. Необходимо выполнить возведение числа x в степень y. Задание необходимо реализовать в виде функции my_func(x, y). При решении задания необходимо обойтись без встроенной функции возведения числа в степень. Подсказка: попробуйте решить задачу двумя способами. Первый — возведение в степень с помощью оператора **. Второй — более сложная реализация без оператора **, предусматривающая использование цикла.

--- Lesson 3/task 4.py
+++ Lesson 3/task 4.py
@@ -4,3 +4,15 @@ def my_func(x, y):
 
 
 my_func(4, 8)
+
+def my_func(x, y):
+    z = 1
+    for i in range(abs(y)):
+        z *= x
+    if y >= 0:
+        return z
+    else:
+        return 1 / z
+
+
+print(my_func(4, -8))
